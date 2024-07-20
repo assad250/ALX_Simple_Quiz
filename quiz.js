365596447f76fb31ab9e6a46d507081e5e16b6a6
Assad Mugisha <assadmugisha@gmail.com>
2024-07-20 15:49:23 +0200

    Developed a quiz app

---
+++ quiz.js
@@ -0,0 +1,21 @@
+function CheckAnswer () {
+    //correct answer
+    const correctAnswer = "4";
+
+    //Retrieve User's answer
+    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+
+    //Get feedback element
+    const feedback = document.getElementById('feedback');
+
+    //Comparing answers and providing feedback
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Welldone ."
+    } else {
+        feedback.textContent = "That's Incorrect. Try Again?"
+    }
+}
+
+
+//adding event listener to the submit button
+document.getElementById('submit-answer').addEventListener('click', CheckAnswer);
